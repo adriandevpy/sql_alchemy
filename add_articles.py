1aad546d7fe65fd171c4abffbdc028af824c5af6
Uzytkownik <uzytkownik@kursGIT.pl>
2023-05-20 12:34:11 +0200

    adding articles to db

---
+++ add_articles.py
@@ -0,0 +1,23 @@
+from session import session
+from models import Author, Article
+from faker import Faker
+
+
+def main():
+    author = session.query(Author).filter_by(
+        user_name="sammy"
+    ).one()
+
+    fake = Faker()
+    article = Article(
+        title=fake.sentence(),
+        content="New article content"
+
+    )
+
+    author.articles.append(article)
+    session.commit()
+
+
+if __name__ == '__main__':
+    main()
